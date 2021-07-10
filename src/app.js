5683000fffef3bd36189c58b92de81d658a93ae1
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2021-07-10 18:40:23 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -16,4 +16,6 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen( 3000 );
+const PORT = process.env.PORT || 3000;
+// server.listen( 3000 );
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
